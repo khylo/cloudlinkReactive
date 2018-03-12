dd75022e177fd09f0f2b0df16a953d7947a6142a
khylo <keith_hyland@yahoo.com>
2018-03-12 16:47:58 +0000

    Add files via upload

---
+++ cloudLinkReactive/src/main/java/com/khylo/cloudlink/CloudLinkReactiveApplication.java
@@ -0,0 +1,12 @@
+package com.khylo.cloudlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudLinkReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudLinkReactiveApplication.class, args);
+	}
+}
